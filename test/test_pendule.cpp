122408d315cf9124396359f0cadacf1ea2c7bc21
Franco Fusco on Rasperry Pi4 <francofusco93@gmail.com>
2021-02-17 19:57:12 +0100

    Basic pendulum class written

---
+++ test/test_pendule.cpp
@@ -0,0 +1,55 @@
+#include "pendule_pi/pigpio.hpp"
+#include "pendule_pi/pendule.hpp"
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <thread>
+#include <cmath>
+
+
+int main(int argc, char** argv) {
+  namespace pp = pendule_pi;
+  try {
+    // Let the token manage the pigpio library
+    pigpio::ActivationToken token;
+    // Create the encoder
+    pp::Pendule pendule(1.0, 2*M_PI/1000, 0.0);
+    std::cout << "Calibrating pendulum" << std::endl;
+    pendule.calibrate();
+    std::cout << "Calibration completed!" << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    bool to_the_right = true;
+    const int COMMAND = 75;
+    double MAX_POSITION = 7000.0;
+    const int SLEEP_MS = 20;
+    std::cout << "    POSITION        ANGLE       LIN.VEL.      ANG.VEL." << std::endl;
+                //  +000000.0000  +000000.0000  +000000.0000  +000000.0000
+    std::cout << std::setfill('0') << std::internal << std::showpos << std::fixed << std::setprecision(4);
+    pendule.setCommand(COMMAND);
+    while(true) {
+      // state estimation
+      std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_MS));
+      pendule.update(SLEEP_MS/1000.0);
+      // Compute command
+      if(to_the_right && pendule.position() > MAX_POSITION) {
+        pendule.setCommand(-COMMAND);
+        to_the_right = false;
+      }
+      else if(!to_the_right && pendule.position() < -MAX_POSITION) {
+        pendule.setCommand(COMMAND);
+        to_the_right = true;
+      }
+      // print on screen
+      std::cout << "\r";
+      std::cout << "  " << std::setw(12) << pendule.position();
+      std::cout << "  " << std::setw(12) << pendule.angle();
+      std::cout << "  " << std::setw(12) << pendule.linearVelocity();
+      std::cout << "  " << std::setw(12) << pendule.angularVelocity();
+      std::cout << "   " << std::flush;
+    }
+  }
+  catch(const pigpio::ActivationToken::PleaseStop&) { }
+  catch(...) { throw; }
+
+  return 0;
+}
---
+++ test/test_pendule_calibration.cpp
@@ -0,0 +1,42 @@
+#include "pendule_pi/pendule.hpp"
+#include "pendule_pi/pigpio.hpp"
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <thread>
+#include <cmath>
+
+
+int main(int argc, char** argv) {
+  namespace pp = pendule_pi;
+  try {
+    // Let the token manage the pigpio library
+    pigpio::ActivationToken token;
+    // create the components as pointer
+    auto motor = std::make_unique<pp::Motor>(24, 16);
+    auto right_switch = std::make_unique<pp::Switch>(18, pp::Switch::NORMALLY_UP, pp::Switch::WITH_PULL_RESISTOR);
+    auto left_switch = std::make_unique<pp::Switch>(17, pp::Switch::NORMALLY_UP, pp::Switch::WITH_PULL_RESISTOR);
+    auto position_encoder = std::make_unique<pp::Encoder>(20, 21);
+    auto angle_encoder = std::make_unique<pp::Encoder>(19, 26);
+    // Create the encoder with the list of components
+    pp::Pendule pendule(
+      1.0, 2*M_PI/1000, 0.0,
+      std::move(motor),
+      std::move(left_switch),
+      std::move(right_switch),
+      std::move(position_encoder),
+      std::move(angle_encoder)
+    );
+    // calibrate the pendulum, then print some data
+    std::cout << "Calibrating pendulum" << std::endl;
+    pendule.calibrate();
+    std::cout << "Calibration completed!" << std::endl;
+    std::cout << "Min steps: " << pendule.minPositionSteps() << std::endl;
+    std::cout << "Mid steps: " << pendule.midPositionSteps() << std::endl;
+    std::cout << "Max steps: " << pendule.maxPositionSteps() << std::endl;
+  }
+  catch(const pigpio::ActivationToken::PleaseStop&) { }
+  catch(...) { throw; }
+
+  return 0;
+}
