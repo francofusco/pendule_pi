0ad2746a2193a129a3729e9c8db3556225479250
Franco Fusco <francofusco93@gmail.com>
2021-06-09 15:06:04 +0200

    script to print the current state in a console

---
+++ src/bin/print_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from pendule_pi import PendulePy
+import time
+import sys
+
+if __name__ == '__main__':
+  host = sys.argv[1] if len(sys.argv) > 1 else "localhost"
+  pendulum = PendulePy(wait=5, host=host)
+
+  print(f" POSITION       ANGLE      LIN.VEL.     ANG.VEL.")
+  # print(f"12345678901  12345678901  12345678901  12345678901")
+
+  try:
+    while True:
+      pendulum.readState(blocking=True)
+      print(f"{pendulum.position:0=11.5f}  {pendulum.angle:0=11.5f}  {pendulum.linvel:0=11.5f}  {pendulum.angvel:0=11.5f}", end='\r', flush=True)
+  except KeyboardInterrupt:
+    pass
